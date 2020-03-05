e358c2cdb36181ac41b443b48265871d248c8024
Billy Ferrara <willfira717@gmail.com>
2020-03-05 19:48:00 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -11,9 +11,7 @@ class MusicLibraryController
     while input != "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
-      list_songs
       puts "To list all of the artists in your library, enter 'list artists'."
-      list_artists
       puts "To list all of the genres in your library, enter 'list genres'."
       puts "To list all of the songs by a particular artist, enter 'list artist'."
       puts "To list all of the songs of a particular genre, enter 'list genre'."
